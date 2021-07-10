40e4025f4fa3e04a6e933dac9be219d362aba22b
Nauyis_ <57891166+zsy326@users.noreply.github.com>
2021-07-10 13:05:36 +0800

    Add files via upload

--- seller/src/components/Home.vue
+++ seller/src/components/Home.vue
@@ -1,90 +1,94 @@
 <template>
   <div class="home-container">
-    <el-container class=biggest-box>
-  <el-header class=header-box>
- <div class=header-div>
-        <img class=logo src="../assets/images/logo.jpg" alt="" />
-        <span>商家管理系统</span>
-        <span id="ID">Seller Management System</span>
-      </div>
-      <div>
-        <div id="user-name">欢迎您：TJ影城   ID:01001</div>
-        <el-dropdown trigger="click">
-          <span class="el-dropdown-link d-flex">
-            <img id="user-avatar" src="../assets/images/avatar.png" alt="" />
-            <i class="el-icon-arrow-down el-icon--right"></i>
-          </span>
-        </el-dropdown>
-      </div>
-  </el-header>
+    <el-container class="biggest-box" height="100%">
+      <el-aside class="aside-box" width="280px" height="900px">
+        <div class="logo">
+          <img src="../assets/images/logo.jpg" alt="" />
+        </div>
 
-  <el-container height="100%">
-    <el-aside class=aside-box width="280px" height="900px">
-       <el-menu class=menu-box >
-      <!--侧边导航栏-->
-      <el-submenu index="1">
-        
-        <template slot="title"><i class="el-icon-s-data"></i>首页</template>
-           <router-link to="/outline">
+        <el-menu class="menu-box">
+          <!--侧边导航栏-->
+          <el-submenu index="1">
+            <template slot="title"><i class="el-icon-s-data"></i>首页</template>
+            <router-link to="/outline">
               <el-menu-item index="1-1">概要信息</el-menu-item>
             </router-link>
-          
-      </el-submenu>
-      <el-submenu index="2">
-        <template slot="title"><i class="el-icon-menu"></i>已发布</template>
-          
-           <router-link to="/slotList">
-            <el-menu-item index="2-1">演出列表</el-menu-item>
-          </router-link>
-          
-      </el-submenu>
-      <el-submenu index="3">
-        <template slot="title"><i class="el-icon-upload2"></i>我要发布</template>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title"><i class="el-icon-menu"></i>已发布</template>
 
-         <router-link to="/releaseSlot">
-            <el-menu-item index="3-1">发布演出</el-menu-item>
-          </router-link>
-      
-          <router-link to="/releaseGoods">
-            <el-menu-item index="3-2">发布周边</el-menu-item>
-          </router-link>
+            <router-link to="/slotList">
+              <el-menu-item index="2-1">演出列表</el-menu-item>
+            </router-link>
+          </el-submenu>
+          <el-submenu index="3">
+            <template slot="title"
+              ><i class="el-icon-upload2"></i>我要发布</template
+            >
 
-      </el-submenu>
-      <el-submenu index="4">
-        <template slot="title"><i class="el-icon-message-solid"></i>历史通知</template>  
-          <router-link to="/notification">
-             <el-menu-item index="4-1">通知列表</el-menu-item>
-          </router-link>
-      </el-submenu>
-      <el-submenu index="5">
-        <template slot="title"><i class="el-icon-setting"></i>编辑资料</template>
+            <router-link to="/releaseSlot">
+              <el-menu-item index="3-1">发布演出</el-menu-item>
+            </router-link>
+
+            <router-link to="/releaseGoods">
+              <el-menu-item index="3-2">发布周边</el-menu-item>
+            </router-link>
+          </el-submenu>
+          <el-submenu index="4">
+            <template slot="title"
+              ><i class="el-icon-message-solid"></i>历史通知</template
+            >
+            <router-link to="/notification">
+              <el-menu-item index="4-1">通知列表</el-menu-item>
+            </router-link>
+          </el-submenu>
+          <el-submenu index="5">
+            <template slot="title"
+              ><i class="el-icon-setting"></i>编辑资料</template
+            >
+
+            <router-link to="/editInfo">
+              <el-menu-item index="5-1">填写资料</el-menu-item>
+            </router-link>
+          </el-submenu>
+          <el-submenu index="6">
+            <template slot="title"
+              ><i class="el-icon-s-order"></i>查看订单</template
+            >
+            <router-link to="/orderList">
+              <el-menu-item index="6-1">订单列表</el-menu-item>
+            </router-link>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+
+      <el-container>
+        <!-- <el-header> -->
+          <el-header type="flex" class="row-bg" justify="space-between">
+            <el-col :span="6"
+              ><div>
+                <span id="ID">商家管理系统</span>
+              </div></el-col
+            >
+            <el-col :span="26"
+              ><div>
+                <div id="user-name">欢迎您：TJ影城 ID:01001</div>
+                </div
+            ></el-col>
+
+             <el-col :span="1"
+>
+                <img
+                      id="user-avatar"
+                      src="../assets/images/avatar.png"
+                      alt="">
+            </el-col>
+          </el-header>
           
-           <router-link to="/editInfo">
-            <el-menu-item index="5-1">填写资料</el-menu-item>
-          </router-link>
-          
-      </el-submenu>
-      <el-submenu index="6">
-        <template slot="title"><i class="el-icon-s-order"></i>查看订单</template>
-          <router-link to="/orderList">
-            <el-menu-item index="6-1">订单列表</el-menu-item>
-          </router-link>
-            
-            
-      </el-submenu>
-    </el-menu>
-    </el-aside>
-    <el-container>
-      <el-main>
-
-        <router-view>
-    </router-view>
-
-      </el-main>
-      <el-footer>Footer</el-footer>
+        <router-view> </router-view>
+        <el-footer>Footer</el-footer>
+      </el-container>
     </el-container>
-  </el-container>
-</el-container>
   </div>
 </template>
 
@@ -95,48 +99,29 @@ export default {};
 
 
 <style Lang='less' scoped>
-
-.home-container{
-  height:100%
+.home-container {
+  height: 100%;
 }
+
 .el-header {
-  background-color: #ffffff;
+  height: 20px;
+  background-color: #df5f39d0;
   display: flex;
   justify-content: space-between;
   align-items: center;
+  margin-bottom: 15px;
 }
- .header-div{
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    color: rgb(252, 252, 252);
-    font-size: 20px;
-    font-weight: bold;
- }
- img {
-      width: 280px;
-      height: 66px;
-      margin-right: 45px;
-    }
-
-  #ID {
-      font-weight: normal;
-      font-size: 16px;
-      margin-left: 0px;
-    }
-
-.el-main {
-  position: relative;
-  background-color: #ffffff;
+img {
+  width: 280px;
+  height: 70px;
+  /* margin-right: 45px; */
 }
 
-
-.header-box{
-  background-color: rgb(73, 88, 119);
-}
-
-.aside-box{
-  background-color: rgb(173, 169, 169);
+#ID {
+  color:rgb(255, 255, 255);
+  font-weight: bold;
+  font-size: 20px;
+  margin-left: 20px;
 }
 #user-avatar {
   width: 45px;
@@ -144,10 +129,20 @@ export default {};
   border-radius: 10px;
   margin-right: 0;
 }
-#user-name{
-  font-weight: normal;
-  margin-right: 55px;
+#user-name {
+  color:rgb(255, 255, 255);
+  font-weight: bold;
   font-size: 20px;
-  color: #ffffff;
+  margin-left: 0px;
 }
+
+.el-main {
+  position: relative;
+  background-color: #b5b7c0;
+}
+
+.aside-box {
+  background-color: rgb(248, 248, 248);
+}
+
 </style>
--- seller/src/components/ReleaseGoods.vue
+++ seller/src/components/ReleaseGoods.vue
@@ -1,15 +1,222 @@
 <template>
-    <div>
-        这里是发布周边的界面
-    </div>
+  <div>
+  <div class="note" >
+   请填写您将要发布的周边的相关信息噢！ 
+  </div>
+    <el-form
+      :model="ruleForm"
+      :rules="rules"
+      ref="ruleForm"
+      label-width="150px"
+      class="demo-ruleForm"
+    >
+     <el-form-item label="对应的演出ID" prop="slotID">
+        <el-input v-model="ruleForm.slotID"></el-input>
+      </el-form-item>
+
+
+      <el-form-item label="周边名称" prop="name">
+        <el-input v-model="ruleForm.name"></el-input>
+      </el-form-item>
+
+      <el-form-item label="周边简介" prop="introduction">
+       
+      
+      <el-input
+  type="textarea"
+  :rows="4"
+  placeholder="请输入简介"
+  v-model="ruleForm.introduction">
+</el-input>
+
+      </el-form-item>
+
+      <el-form-item label="价格（RMB）" prop="price">
+        <el-input-number
+          v-model="price"
+          controls-position="right"
+          @change="handleChange4"
+          :min="0"
+        ></el-input-number>
+      </el-form-item>
+
+      <el-form-item label="上传实物图/平面图" prop="areaNum">
+        <el-upload
+          class="avatar-uploader"
+          action="https://jsonplaceholder.typicode.com/posts/"
+          :show-file-list="false"
+          :on-success="handleAvatarSuccess"
+          :before-upload="beforeAvatarUpload"
+        >
+          <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+          <i v-else class="el-icon-plus goods-uploader-icon"></i>
+        </el-upload>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "ReleaseSlot",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      ruleForm: {
+        name: "喵喵",
+        region: "",
+        date1: "",
+        date2: "",
+        delivery: false,
+        type: [],
+        resource: "",
+        desc: "",
+      },
+      value: [],
+      price: 0,
+      rules: {
+        slotID: [
+          { required: true, message: "请输入对应的演出ID", trigger: "blur" },
+          {
+            min: 1,
+            max: 10,
+            message: "长度在 1 到 10 个字符",
+            trigger: "blur",
+          },
+        ],
+        name: [
+          { required: true, message: "请输入周边名称", trigger: "blur" },
+          {
+            min: 1,
+            max: 50,
+            message: "长度在 1 到 50 个字符",
+            trigger: "blur",
+          },
+        ],
+        introduction: [
+          { required: true, message: "请输入周边简介", trigger: "blur" },
+          {
+            min: 1,
+            max: 4000,
+            message: "长度在 1 到 4000 个字符",
+            trigger: "blur",
+          },
+        ],
+        address: [
+          { required: true, message: "请输入演出地址", trigger: "blur" },
+          {
+            min: 1,
+            max: 20,
+            message: "长度在 1 到 20 个字符",
+            trigger: "blur",
+          },
+        ],
+        tag1: [
+          { required: true, message: "请选择一个标签", trigger: "change" },
+        ],
+        date1: [
+          {
+            type: "date",
+            required: true,
+            message: "请选择日期",
+            trigger: "change",
+          },
+        ],
+        date2: [
+          {
+            type: "date",
+            required: true,
+            message: "请选择时间",
+            trigger: "change",
+          },
+        ],
+      },
+      imageUrl: "",
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+    handleChange1(value) {
+      console.log1(value);
+    },
+    handleChange2(value) {
+      console.log2(value);
+    },
+    handleChange3(value) {
+      console.log3(value);
+    },
+    handleChange4(value) {
+      console.log4(value);
+    },
+    handleAvatarSuccess(res, file) {
+      this.imageUrl = URL.createObjectURL(file.raw);
+    },
+    beforeAvatarUpload(file) {
+      const isJPG = file.type === "image/jpeg";
+      const isLt5M = file.size / 1024 / 1024 < 5;
 
+      if (!isJPG) {
+        this.$message.error("上传座位图片只能是 JPG 格式!");
+      }
+      if (!isLt5M) {
+        this.$message.error("上传座位图片大小不能超过 5MB!");
+      }
+      return isJPG && isLt5M;
+    },
+  },
+};
 </script>
 
-
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.goods-uploader .el-upload {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+.avatar-uploader .el-upload:hover {
+  border-color: #409eff;
+}
+.goods-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  background-color: rgba(175, 180, 201, 0.192);
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
+}
+.avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
+
+.note {
+  margin-left: 10px;
+  margin-bottom: 20px;
+  color: rgb(49, 168, 189);
+  font-size: 20px;
+  font-weight: bold;
+}
+.el-input {
+  width: 600px;
+}
 
 </style>
